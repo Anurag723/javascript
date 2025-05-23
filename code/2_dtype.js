3d72f553d7241389fbefe72c75e69d54a2825dfc
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-23 18:13:18 +0000

    various dtypes

--- code/2_dtype.js
+++ code/2_dtype.js
@@ -11,3 +11,12 @@ console.log(typeof(name));
 console.log(typeof(state));
 console.log(typeof undefined);
 console.log(typeof null);
+
+
+// number=>2**53
+// bigint
+// string ""
+// Boolean => true/false
+// null => object/standalone value
+// undefined =>
+// symbol => unique
