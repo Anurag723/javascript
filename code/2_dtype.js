6bdf2a88f4b61d1f09ac801da3e84d09bdb932d3
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-23 18:08:24 +0000

    typeof variables in java as ECMA standard

---
+++ code/2_dtype.js
@@ -0,0 +1,13 @@
+"use strict";       //treat all js code as newer version
+
+let name = "Anurag";
+let age = 22;
+let isloggedin = false;
+let state;
+
+console.table([name,age,isloggedin,state]);
+
+console.log(typeof(name));
+console.log(typeof(state));
+console.log(typeof undefined);
+console.log(typeof null);
