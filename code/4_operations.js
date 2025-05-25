9cc8e18af4d56fab03408fe3b0e305e3e9b691bb
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-25 18:45:43 +0000

    sum operations

---
+++ code/4_operations.js
@@ -0,0 +1,4 @@
+console.log("1"+2);     //gives 12 treating entire as string
+console.log(1 + "2");   //same as previous
+console.log("1" + 2 + 2)    //gives 122 treating all as string
+console.log(1 + 2 +"2")     //gives 32 as first two work as int rem are string
